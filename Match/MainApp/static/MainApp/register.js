816c2040cbdcfaedbca4acfeadedd9c36e6db7eb
Ramim96 <34135757+Ramim96@users.noreply.github.com>
2018-11-15 13:06:13 +0000

    Updated version for checkboxes

--- Match/MainApp/static/MainApp/register.js
+++ Match/MainApp/static/MainApp/register.js
@@ -1,37 +1,41 @@
 $(document).ready(function() {
-  $('#registerForm').submit(function() {
+  $('#register').click(function(e) {
+    e.preventDefault();
+
+    var hobbies = [];
+
     var fName = $('#firstName').val();
     var lName = $('#lastName').val();
     var age = $('#age').val();
     var dob = $('#dob').val();
-    var gender = $("name=['gender']").val();
+    var gender = $('input[type=radio]:checked').val();
     var email = $('#email').val();
     var pwd = $('#pwd').val();
+    var pic = $('#profilePic').val();
+
+    $.each($("input[name='hobby']:checked"), function() {
+      hobbies.push($(this).val());
 
     ajaxReq = {
       url: '/newUser/',
       type: 'POST',
       dataType: 'json',
-      data: {
-        'fName': fName,
-        'lName': lName,
-        'age': age,
-        'dob': dob,
-        'gender': gender,
-        'email': email,
-        'pwd': pwd,
-        //'pic': $('#profilePic').val()
-      },
+      data: { 'fName': fName,
+              'lName': lName,
+              'age': age,
+              'dob': dob,
+              'gender': gender,
+              'email': email,
+              'pwd': pwd,
+              'pic': pic
+            },
       success: function(data) {
-        for (var i = 0; i < data.length; i++) {
-          alert(data[i]);
-        }
+        alert("User profile created !");
       }
       error: function() {
-        alert("Something went wrong !");
+        alert("Something went wrong during the process !");
       }
     }
-
-    $.ajax(ajaxReq);
+    });
   });
 });
